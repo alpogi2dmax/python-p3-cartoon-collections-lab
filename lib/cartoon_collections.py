1fd864d52c1a4d14a03c325b4570d356a9f1d262
Al Cerdan Lico <alcerdan@yahoo.com>
2024-11-21 11:36:03 -0800

    complete python cartoon collections lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,24 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves_list):
+    for i in range(len(dwarves_list)):
+        print(f"{i+1}. {dwarves_list[i]}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteers):
+    new_planeteers = list()
+    for planeteer in planeteers:
+        new_planeteers.append(f"{planeteer.capitalize()}!")
+    return new_planeteers
 
-def long_planeteer_calls():
-    pass
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(words):
+    return len(words) == 4
+
+def find_the_cheese(foods):
+    for food in foods:
+        if food == 'cheddar':
+            return 'cheddar'
+        elif food == 'gouda':
+            return 'gouda'
+        elif food == 'camembert':
+            return 'camembert'
+        else:
+            'None'
